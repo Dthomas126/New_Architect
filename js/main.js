58b9f186bf979536abe68ad4cafda16f3279cfc1
Devin Thomas <dlt95@txstate.edu>
2019-01-27 10:49:37 -0600

    initial submit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var navButton = $('.nav-button');
+    var close = $('.nav-close')
+    var nav = $('#nav');
+    
+
+  navButton.click(function(){
+   
+        nav.css("width","40%");
+ 
+  });
+
+  close.click(function(){
+    
+        nav.css("width","0px");
+  
+  });
+
+
+  
+   
+
+
+
+     
+    
+
+
+
+});
